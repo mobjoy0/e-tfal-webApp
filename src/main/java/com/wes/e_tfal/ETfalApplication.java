85b8c1cfe5fd424915876136b677aa284bea8cbc
mobjoy0 <wessimbenna@hotmail.com>
2025-01-01 00:05:18 +0100

    inital commit

---
+++ src/main/java/com/wes/e_tfal/ETfalApplication.java
@@ -0,0 +1,13 @@
+package com.wes.e_tfal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ETfalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ETfalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wes/e_tfal/ETfalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wes.e_tfal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ETfalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
